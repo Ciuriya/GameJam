774488a82bd6b6a16c68d574477adacd2bad5c82
Max <maxime___tremblay@hotmail.com>
2018-10-06 02:21:39 -0400

    Ajout du menu d'options

--- WonderJam/Assets/Code/Components/MenuHandler.cs
+++ WonderJam/Assets/Code/Components/MenuHandler.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuHandler : MonoBehaviour
 {
@@ -14,25 +15,41 @@ public class MenuHandler : MonoBehaviour
 	[Tooltip("The game event raised when the game is resumed.")]
 	public GameEvent m_resumeEvent;
 
+	[Tooltip("The game event raised when the menu changes.")]
+	public GameEvent m_onMenuChangedEvent;
+
 	private void Update()
 	{
 		bool isPaused = m_currentMenu;
 
 		if(Input.GetButtonDown("Cancel") && !isPaused)
 			m_pauseEvent.Raise();
-		else if(Input.GetButtonDown("Cancel") && isPaused)
+		else if(Input.GetButtonDown("Cancel") && isPaused && !m_previousMenu)
 			m_resumeEvent.Raise();
+		else if(Input.GetButtonDown("Cancel") && isPaused)
+			ChangeMenu(m_previousMenu);
 	}
 
 	public void ChangeMenu(GameObject p_menu)
 	{
-		m_previousMenu = m_currentMenu;
+		if(m_currentMenu) m_currentMenu.SetActive(false);
+
+		if(m_previousMenu == p_menu) m_previousMenu = null;
+		else m_previousMenu = m_currentMenu;
+
 		m_currentMenu = p_menu;
+
+		m_currentMenu.SetActive(true);
+
+		m_onMenuChangedEvent.Raise();
 	}
 
 	public void ClearMenu()
 	{
+		if(m_currentMenu) m_currentMenu.SetActive(false);
+
 		m_currentMenu = null;
+		m_previousMenu = null;
 	}
 
 	public void Pause()
@@ -43,5 +60,10 @@ public class MenuHandler : MonoBehaviour
 	public void Resume()
 	{
 		Time.timeScale = 1f;
+	}
+
+	public void OnVolumeChanged(float p_value)
+	{
+		AudioListener.volume = p_value;
 	}
 }
