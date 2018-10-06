449585bae32bd67afec072b9d4770905b01226c1
Max <maxime___tremblay@hotmail.com>
2018-10-06 04:36:50 -0400

    Added enemy base class + drop tables

---
+++ WonderJam/Assets/Code/Components/UnitHealth.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class UnitHealth : MonoBehaviour
+{
+	[Tooltip("Unit's health")]
+	public float m_health;
+
+	[Tooltip("Unit's max health")]
+	public float m_maxHealth;
+
+	[Tooltip("Event called when the entity dies.")
+	public UnityEvent m_deathEvent;
+
+	public void Damage(float p_amount)
+	{
+		m_health -= p_amount;
+
+		if(m_health <= 0.0f)
+			m_deathEvent.Invoke();
+	}
+}
