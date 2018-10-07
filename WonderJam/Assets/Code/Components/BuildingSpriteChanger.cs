c4d80b7b873b34f913e0bd4f93e04e941e5bc6d2
Unknown <jason_589@live.ca>
2018-10-06 20:59:12 -0400

    Just begun to work with the sprite and color changing

---
+++ WonderJam/Assets/Code/Components/BuildingSpriteChanger.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingSpriteChanger : MonoBehaviour {
+
+
+    public Sprite m_summerSprite;
+    public Sprite m_winterSprite;
+    public GameEvent seasonChangeEvent;
+    public SeasonVariable m_currentSeason;
+    public Season m_seasonSpring;
+    public Season m_seasonSummer;
+    public Season m_seasonFall;
+    public Season m_seasonWinter;
+
+    //***WARNING THIS CODE MIGHT MAKE YOU THROW UP A LITTLE IN YOUR MOUTH***
+    public void ChangeSprite()
+    {
+        //LoadSprite to change
+
+        //Load summer sprite and apply color to it
+        if (m_currentSeason.Value == m_seasonSpring)
+        {
+
+        }
+
+        //Load summer sprite witch summercolor
+        if (m_currentSeason.Value == m_seasonSummer)
+        {
+
+        }
+
+        //Load summer sprite and apply color
+        if (m_currentSeason.Value == m_seasonFall)
+        {
+
+        }
+
+        //Load Winter sprite With base color
+        if (m_currentSeason.Value == m_seasonWinter)
+        {
+
+        }
+
+    }
+
+    
+}
