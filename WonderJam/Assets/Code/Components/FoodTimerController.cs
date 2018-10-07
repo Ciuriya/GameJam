13aa82ea00e0d5c38ed800c506cb1e1178a99d44
Marko <mcyx03@gmail.com>
2018-10-06 23:31:26 -0400

    Add food system
    still need testing

--- WonderJam/Assets/Code/Components/FoodTimerController.cs
+++ WonderJam/Assets/Code/Components/FoodTimerController.cs
@@ -5,27 +5,39 @@ using UnityEngine.UI;
 
 public class FoodTimerController : MonoBehaviour
 {
-    private const float HEALTH_LOST = 0.5;
+    private const float HEALTH_LOST = 0.05f;
     private const float BASE_FOOD_LOST = 5; //qty of food lost every second;
-
+    private const float STARVATION_THICK_DELAY = 1;
+    private float lastHit;
 
     public FloatReference currentFood;
-    //public Game    
+    public FloatReference maxHealth;
+    UnitHealth unitHealth;
+    //GameObject damager;   
 
     // Use this for initialization
     void Start()
     {
+        lastHit = 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (currentFood == 0f)
+        if (currentFood.Value == 0f)
         {
-            currentFood = currentFood - (BASE_FOOD_LOST / Time.deltaTime);
-            if (currentFood < 0)
+            currentFood.m_variable.Value = currentFood.Value - (BASE_FOOD_LOST * Time.deltaTime);
+            if (currentFood.Value < 0)
             {
-                currentFood = 0;
+                currentFood.m_variable.Value = 0;
+            }
+        }
+        else
+        {
+            if (lastHit - STARVATION_THICK_DELAY <= 0) ;
+            {
+                lastHit = Time.time;
+                unitHealth.Starvation(maxHealth.Value * HEALTH_LOST);
             }
         }
     }
