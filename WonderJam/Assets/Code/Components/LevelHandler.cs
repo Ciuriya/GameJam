3acaa98c7eb4e0bf4410a2d21f0ca6e1dec38e23
Max <maxime___tremblay@hotmail.com>
2018-10-07 01:55:15 -0400

    Ajout de la liste de levels et de levels visités

---
+++ WonderJam/Assets/Code/Components/LevelHandler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelHandler : MonoBehaviour
+{
+	public LevelsRuntimeSet m_levels;
+	public LevelsRuntimeSet m_levelsVisited;
+
+	public void LoadRandomLevel()
+	{
+		string level = null;
+
+		while(level == null && m_levels.Length() >= m_levelsVisited.Length())
+		{
+			string temp = m_levels.m_items[Random.Range(0, m_levels.Length() - 1)];
+
+			if(!m_levelsVisited.Contains(temp))
+			{
+				level = temp;
+			}
+		}
+
+		m_levelsVisited.Add(level);
+
+		SceneManager.LoadScene(level);
+	}
+
+	public void RestartLevel()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+}
---
+++ WonderJam/Assets/Code/Variables/Sets/LevelsRuntimeSet.cs
@@ -0,0 +1,4 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu (menuName = "Sets/Levels")]
+public class LevelsRuntimeSet : RuntimeSet<string> {}
