38e980e0267add65ef1468e270fc7e6769b1fa26
Etienne <etienneemond97@hotmail.com>
2018-10-07 14:46:56 -0400

    Auto stash before merge of "develop" and "origin/develop"

---
+++ WonderJam/Assets/Indicateur.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Indicateur : MonoBehaviour
+{
+    public Text indicateur;
+    public FloatVariable valueRef;
+    public FloatVariable maxValueRef;
+
+    public void OnEnable()
+    {
+        UpdateIndicateur();
+    }
+
+    public void UpdateIndicateur()
+    {
+        indicateur.text = valueRef.Value + "/" + maxValueRef.Value;
+    }
+}
