de80c4d4b82e9c58544bae09f4e0b18417ac5252
Marko <mcyx03@gmail.com>
2018-10-06 21:15:59 -0400

    [WIP] Started food controller

---
+++ WonderJam/Assets/Code/Components/FoodTimerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FoodTimerController : MonoBehaviour
+{
+    private const float HEALTH_LOST = 0.5;
+    private const float BASE_FOOD_LOST = 5; //qty of food lost every second;
+
+
+    public FloatReference currentFood;
+    //public Game    
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentFood == 0f)
+        {
+            currentFood = currentFood - (BASE_FOOD_LOST / Time.deltaTime);
+            if (currentFood < 0)
+            {
+                currentFood = 0;
+            }
+        }
+    }
+}
+
