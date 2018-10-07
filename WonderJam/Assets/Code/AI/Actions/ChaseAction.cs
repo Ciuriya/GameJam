c347789c6680bfd108401ac8f72e6fc087995517
Maxime <maxime_morette@hotmail.com>
2018-10-06 21:03:39 -0400

    LE PATROL MARCHE!!!!!!!!!!!!

--- WonderJam/Assets/Code/AI/Actions/ChaseAction.cs
+++ WonderJam/Assets/Code/AI/Actions/ChaseAction.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 [CreateAssetMenu(menuName = "AI/Actions/Chase")]
 
 public class ChaseAction : Action {
+
+    public Transform chaseTarget;
+    int MinDist = 5;
+    int MaxDist = 10;
+    int MoveSpeed = 4;
 
     public override void Execute(StateController controller)
     {
@@ -14,6 +20,17 @@ public class ChaseAction : Action {
 
     private void Chase(StateController controller)
     {
+        controller.transform.LookAt(chaseTarget);
 
+        if(Vector2.Distance(controller.transform.position, chaseTarget.position) >= MinDist)
+        {
+            controller.transform.position += controller.transform.forward * MoveSpeed * Time.deltaTime;
+
+            if (Vector2.Distance(controller.transform.position, chaseTarget.position) <= MaxDist)
+            {
+
+            }
+        }
     }
+
 }
--- WonderJam/Assets/Code/AI/Actions/PatrolAction.cs
+++ WonderJam/Assets/Code/AI/Actions/PatrolAction.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
+using UnityEngine.UI;
 
 
 [CreateAssetMenu(menuName = "AI/Actions/Patrol")]
 public class PatrolAction : Action {
 
+    public Transform[] points;
+    public int nextPoint = 0;
+    int MaxDist = 10;
+    int MinDist = 1;
+    int MoveSpeed = 4;
 
 
     public override void Execute(StateController controller)
@@ -16,7 +21,19 @@ public class PatrolAction : Action {
 
     private void Patrol(StateController controller)
     {
-       
+        controller.transform.LookAt(points[nextPoint].position);
+        if(Vector2.Distance (controller.transform.position, points[nextPoint].position) >= MinDist)
+        {
+            controller.transform.position += controller.transform.forward * MoveSpeed * Time.deltaTime;
+            if (Vector2.Distance(controller.transform.position, points[nextPoint].position) <= MaxDist)
+            {
+
+            }
+        }
+        else
+        {
+            nextPoint = (nextPoint + 1) % points.Length;
+        }
     }
 
 }
